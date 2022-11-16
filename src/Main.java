a12a4ee1ab3b8c6ffe7b73952dbf8017a1188fb9
Maria Ustyuzhanina <elkaran@gmail.com>
2022-11-16 22:51:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 20_000;
+        int oneMile = 20;
+
+        int miles = ticket / oneMile;
+
+        System.out.println("Начисленные мили: " + miles);
+    }
+
+   }
